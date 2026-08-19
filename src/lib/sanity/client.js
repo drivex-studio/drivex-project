72b6fecbbdc24984a8a3a2cb28457b753195f868
Ye Min Thu <245580708+ymt-nk40@users.noreply.github.com>
2026-08-19 20:45:36 +0630

    Add missing 'use client' directives

---
+++ src/lib/sanity/client.js
@@ -0,0 +1,9 @@
+import { createClient } from "next-sanity";
+import { env } from "env";
+
+export const sanityClient = createClient({
+  projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: env.NEXT_PUBLIC_SANITY_API_VERSION,
+  useCdn: process.env.NODE_ENV === "production",
+});
