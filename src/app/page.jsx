7f8b547cda4befeedd247486f5194e1f28452d8f
drivex-studio <300102742+drivex-studio@users.noreply.github.com>
2026-08-23 17:58:19 +0630

    page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
-import HeroSection from "@home/sections/HeroSection";
-import CardsSection from "@home/sections/CardsSection";
+import HeroSection from “@home/sections/HeroSection”;
+import CardsSection from “@home/sections/CardsSection”;
 import AnimatedListSection from “@home/sections/AnimatedListSection”;
-import FeaturedWorkSection from "@home/sections/FeaturedWorkSection";
-import IndexedGridSection from "@home/sections/IndexedGridSection";
+import FeaturedWorkSection from “@home/sections/FeaturedWorkSection”;
+import IndexedGridSection from “@home/sections/IndexedGridSection”;
 
 export default function HomePage() {
   return (
